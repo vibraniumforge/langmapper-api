ad496862e7ca62798ab2bad25ab9a033bd837fa0
Gregory Morgan <gregm9999@gmail.com>
2024-02-21 21:13:39 -0800

    add 2 new langs

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -1,16 +1,15 @@
 # encoding: utf-8
 class Translation < ApplicationRecord
-
   belongs_to :language
   belongs_to :word
 
   validates :translation, presence: true
   validates :link, presence: true
 
-    # counter
-    def self.translations_count
-      Translation.count
-    end
+  # counter
+  def self.translations_count
+    Translation.count
+  end
 
   # return flattened json for seeds
   def self.seeds
@@ -35,27 +34,26 @@ class Translation < ApplicationRecord
     Translation.joins(:language, :word).select("translations.*, languages.name, languages.macrofamily, words.word_name ").where("etymology LIKE :query", query: "%#{sanitize_sql_like(query)}%")
   end
 
-    # all the translations of EVERY WORD in a MACROFAMILY
-    def self.find_all_translations_by_macrofamily(macrofamily)
-      Translation.joins(:language, :word).select("translations.*, languages.*, languages.id as language_id, translations.id as id, words.word_name as word_name").where("macrofamily = ?", macrofamily).order(:family)
-    end
+  # all the translations of EVERY WORD in a MACROFAMILY
+  def self.find_all_translations_by_macrofamily(macrofamily)
+    Translation.joins(:language, :word).select("translations.*, languages.*, languages.id as language_id, translations.id as id, words.word_name as word_name").where("macrofamily = ?", macrofamily).order(:family)
+  end
 
-    # all the TRANSLATIONS from a specified LANGUAGE
-    def self.find_all_translations_by_language(language)
-      language_id = Language.find_by(name: language).id
-      Translation.joins(:language, :word).select("translations.*, languages.macrofamily, words.word_name").where("language_id = ?", language_id).order(:word_name)
-    end
+  # all the TRANSLATIONS from a specified LANGUAGE
+  def self.find_all_translations_by_language(language)
+    language_id = Language.find_by(name: language).id
+    Translation.joins(:language, :word).select("translations.*, languages.macrofamily, words.word_name").where("language_id = ?", language_id).order(:word_name)
+  end
 
-    # find all the translations of the word_name && are in area1 || area2 || area3.
-    def self.find_all_translations_by_area(area, word_name)
-      word_id = Word.find_by(word_name: word_name.downcase).id
-      Translation.joins(:language, :word).select("translations.*, languages.*, languages.id as language_id, translations.id as id, words.word_name").where("area1 = ?", area).or(Translation.joins(:language, :word).select("translations.*, languages.*, languages.id as language_id, translations.id as id, words.word_name").where("area2 = ?", area)).or(Translation.joins(:language, :word).select("translations.*, languages.*, languages.id as language_id, translations.id as id, words.word_name").where("area3 = ?", area)).where("word_id = ?", word_id).order(:macrofamily, :family, :subfamily)
-    end
+  # find all the translations of the word_name && are in area1 || area2 || area3.
+  def self.find_all_translations_by_area(area, word_name)
+    word_id = Word.find_by(word_name: word_name.downcase).id
+    Translation.joins(:language, :word).select("translations.*, languages.*, languages.id as language_id, translations.id as id, words.word_name").where("area1 = ?", area).or(Translation.joins(:language, :word).select("translations.*, languages.*, languages.id as language_id, translations.id as id, words.word_name").where("area2 = ?", area)).or(Translation.joins(:language, :word).select("translations.*, languages.*, languages.id as language_id, translations.id as id, words.word_name").where("area3 = ?", area)).where("word_id = ?", word_id).order(:macrofamily, :family, :subfamily)
+  end
 
-    def self.find_all_translations_by_area_europe_map(area, word_name)
-      my_europe_svg = ["ab", "ar", "az", "be", "bg", "br", "ca", "co", "cs", "cy", "da", "de", "el", "en", "es", "et", "eu", "fi", "fo", "fr", "fy", "ga", "gag", "gd", "gl", "hu", "hy", "is", "it", "ka", "kk", "krl", "lb", "lij", "lt", "lv", "mk", "mt", "nap", "nl", "no", "oc", "os", "pl", "pms", "pt", "rm", "ro", "ru", "sc", "scn", "sco", "se", "sh", "sh", "sh", "sk", "sl", "sq", "sv", "tk", "tt", "uk", "vnc", "xal"]
-      word_id = Word.find_by(word_name: word_name.downcase).id
-      Translation.joins(:language, :word).select("translations.*, languages.*, languages.id as language_id, translations.id as id, words.word_name").where('languages.abbreviation IN (?)', my_europe_svg ).where("word_id = ?", word_id).order(:macrofamily, :family, :subfamily)
-    end
-
+  def self.find_all_translations_by_area_europe_map(area, word_name)
+    my_europe_svg = ["ab", "ar", "az", "be", "bg", "br", "ca", "co", "cs", "cy", "da", "de", "el", "en", "es", "et", "eu", "fi", "fo", "fr", "fy", "ga", "gag", "gd", "gl", "hu", "hy", "is", "it", "ka", "kk", "krl", "lb", "lij", "lt", "lv", "mk", "mt", "nap", "nl", "no", "oc", "os", "pl", "pms", "pt", "rm", "ro", "ru", "sc", "scn", "sco", "se", "sh", "sh", "sh", "sk", "sl", "sq", "sv", "tk", "tt", "uk", "vnc", "xal", "gv", "kw"]
+    word_id = Word.find_by(word_name: word_name.downcase).id
+    Translation.joins(:language, :word).select("translations.*, languages.*, languages.id as language_id, translations.id as id, words.word_name").where("languages.abbreviation IN (?)", my_europe_svg).where("word_id = ?", word_id).order(:macrofamily, :family, :subfamily)
+  end
 end
