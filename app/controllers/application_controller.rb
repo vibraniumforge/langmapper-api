56ab61f04e6bb6194009f583ea5d0b6231642760
Gregory Morgan <gregm9999@gmail.com>
2024-03-04 13:06:53 -0800

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,41 @@
 class ApplicationController < ActionController::API
+  # before_action :authorized
+
+  def authorized
+    render json: {message: "Please log in", success: false} unless logged_in?
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    if decoded_token
+      user_id = decoded_token[0]["user_id"]
+      @user = User.find(user_id)
+    else
+      puts "no token"
+      nil
+    end
+  end
+
+  def decoded_token
+    if auth_header
+      token = auth_header.split(" ")[1]
+    end
+    begin
+      JWT.decode(token, "my_code", algorithm: "HS256")
+      # JWT.decode => [{ "user_id"=>"2" }, { "alg"=>"HS256" }]
+    rescue JWT::DecodeError
+      nil
+    end
+  end
+
+  def auth_header
+    request.headers["Authorization"] #=> Bearer <token>
+  end
+
+  def encode_token(payload) #{ user_id: 2 }
+    JWT.encode(payload, "my_code") #issue a token, store payload in token
+  end
 end
