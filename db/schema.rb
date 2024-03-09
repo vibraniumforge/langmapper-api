a4061dd4f10191faf2d6cb505bb40315297fb362
Gregory Morgan <gregm9999@gmail.com>
2024-03-08 18:17:43 -0800

    small changes

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_03_151206) do
     t.string "area3"
     t.text "notes"
     t.boolean "has_gender"
+    t.string "flag"
     t.boolean "alive"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -46,7 +47,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_03_151206) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -55,6 +56,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_03_151206) do
   create_table "words", force: :cascade do |t|
     t.string "word_name"
     t.string "definition"
+    t.string "emoji"
+    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
