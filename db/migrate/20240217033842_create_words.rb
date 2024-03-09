f899741e1a23df7ca292ce6472c27ba42f531fb2
Gregory Morgan <gregm9999@gmail.com>
2024-03-08 18:15:57 -0800

    add emoji and category

--- db/migrate/20240217033842_create_words.rb
+++ db/migrate/20240217033842_create_words.rb
@@ -3,6 +3,8 @@ class CreateWords < ActiveRecord::Migration[7.1]
     create_table :words do |t|
       t.string :word_name
       t.string :definition
+      t.string :emoji
+      t.string :category
 
       t.timestamps
     end
