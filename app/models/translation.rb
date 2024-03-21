ed2b484251d4c8b2ec9e23c4272795aeb2b899e0
Gregory Morgan <gregm9999@gmail.com>
2024-03-21 11:18:46 -0700

    remove duplicate 'sh' entries

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -52,13 +52,13 @@ class Translation < ApplicationRecord
   end
 
   def self.find_all_translations_by_area_europe_map(area, word_name)
-    my_europe_svg = ["ab", "ar", "az", "be", "bg", "br", "ca", "co", "cs", "cy", "da", "de", "el", "en", "es", "et", "eu", "fi", "fo", "fr", "fy", "ga", "gag", "gd", "gl", "hu", "hy", "is", "it", "ka", "kk", "krl", "lb", "lij", "lt", "lv", "mk", "mt", "nap", "nl", "no", "oc", "os", "pl", "pms", "pt", "rm", "ro", "ru", "sc", "scn", "sco", "se", "sh", "sh", "sh", "sk", "sl", "sq", "sv", "tk", "tt", "uk", "vnc", "xal", "gv", "kw"]
+    my_europe_svg = ["ab", "ar", "az", "be", "bg", "br", "ca", "co", "cs", "cy", "da", "de", "el", "en", "es", "et", "eu", "fi", "fo", "fr", "fy", "ga", "gag", "gd", "gl", "hu", "hy", "is", "it", "ka", "kk", "krl", "lb", "lij", "lt", "lv", "mk", "mt", "nap", "nl", "no", "oc", "os", "pl", "pms", "pt", "rm", "ro", "ru", "sc", "scn", "sco", "se", "sh", "sk", "sl", "sq", "sv", "tk", "tt", "uk", "vnc", "xal", "gv", "kw"]
     word_id = Word.find_by(word_name: word_name.downcase).id
     Translation.joins(:language, :word).select("translations.*, languages.*, languages.id as language_id, translations.id as id, words.*").where("languages.abbreviation IN (?)", my_europe_svg).where("word_id = ?", word_id).order(:macrofamily, :family, :subfamily)
   end
 
   def self.find_all_translations_by_area_europe_map_small(area, word_name)
-    my_europe_small_svg = [ "ar", "be", "bg", "br", "ca", "co", "cs", "cy", "da", "de", "el", "en", "es", "et", "eu", "fi", "fo", "fr", "fy", "ga", "gag", "gd", "gl", "hu", "is", "it", "krl", "lb", "lij", "lt", "lv", "mk", "mt", "nap", "nl", "no", "oc", "pl", "pms", "pt", "rm", "ro", "ru", "sc", "scn", "sco", "se", "sh", "sh", "sh", "sk", "sl", "sq", "sv", "tk", "uk", "vnc", "gv", "kw"]
+    my_europe_small_svg = [ "ar", "be", "bg", "br", "ca", "co", "cs", "cy", "da", "de", "el", "en", "es", "et", "eu", "fi", "fo", "fr", "fy", "ga", "gag", "gd", "gl", "hu", "is", "it", "krl", "lb", "lij", "lt", "lv", "mk", "mt", "nap", "nl", "no", "oc", "pl", "pms", "pt", "rm", "ro", "ru", "sc", "scn", "sco", "se", "sh", "sk", "sl", "sq", "sv", "tk", "uk", "vnc", "gv", "kw"]
     word_id = Word.find_by(word_name: word_name.downcase).id
     Translation.joins(:language, :word).select("translations.*, languages.*, languages.id as language_id, words.word_name").where("languages.abbreviation IN (?)", my_europe_small_svg).where("word_id = ?", word_id).order(:macrofamily, :family, :subfamily)
   end
